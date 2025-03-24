36e8f92be6d9360775d576ac9b0f367233518d79
NathanESE <nathan.soudarin@etu-upsaclay.fr>
2025-03-24 12:28:32 +0100

    update avec timer pour pwm

--- LIDAR KEIL/baseUART.c
+++ LIDAR KEIL/baseUART.c
@@ -1,10 +1,69 @@
-#include "Driver_USART.h" // ::CMSIS Driver:USART
+#include "Driver_USART.h"
 #include <LPC17XX.h>
-#include "GPIO_LPC17xx.h"               // Keil::Device:GPIO
-
+#include "GPIO_LPC17xx.h"               
+#include "LPC17xx.h"                    
 
 extern ARM_DRIVER_USART Driver_USART0;
 
+void initTimer0(int prescaler, int MR);
+
+void TIMER0_IRQHandler(void);
+
+void Init_UART0(void);
+
+void Send_Lidar_Command(unsigned char command, unsigned char payloadsize, unsigned char payload[]);	
+
+void Temporisation_ms(int a); 													//fonction de temporisation en ms
+
+int main (void){
+	
+	//Variables
+	unsigned char tab[5] = {0x00,0x00,0x00,0x00,0x00};
+	
+	//Initialisations
+	Init_UART0();
+	initTimer0(1999,0);
+	
+		//Init P2.5 en sortie
+	LPC_GPIO2 -> FIODIR0 |= (1<<5); 
+
+	//commande lidar
+	Send_Lidar_Command(0x82,0x05,tab);
+	
+	while(1){
+ 
+	}
+		
+	return 0;
+}
+
+void initTimer0(int prescaler, int MR)
+{
+		// Validation des 4 TIMERS   
+		LPC_SC->PCONP = LPC_SC->PCONP | 0x00C00006;   
+	
+		// Configuration de la période de comptage
+		LPC_TIM0->PR = prescaler;  // le registre PR prend la valeur du prescaler
+		LPC_TIM0->MR0 = MR;    // le registre MR0 prend la valeur maximum du compteur
+		//le compteur, nommé TC ici, est remis à 0 chaque fois qu'il 
+	  //atteint la valeur de //MR0, MR0 est le registre qui contient la valeur de N 
+		// voir la table 429
+		LPC_TIM0->MCR |= 0x00000003;
+	
+		// validation de timer 0 et reset compteur ceci est toujours fait en dernier		
+		LPC_TIM0->TCR = 1; //permet au timer de créer une interruption
+	
+		NVIC_SetPriority(TIMER0_IRQn,10);	
+		NVIC_EnableIRQ(TIMER0_IRQn);
+}
+
+void TIMER0_IRQHandler(void){
+
+	LPC_TIM0 -> IR = (1<<0);
+	
+	LPC_GPIO2 -> FIOPIN0 ^= (1<<5); 
+	
+}
 
 void Init_UART0(void){
 	Driver_USART0.Initialize(NULL);
@@ -18,7 +77,6 @@ void Init_UART0(void){
 	Driver_USART0.Control(ARM_USART_CONTROL_TX,1);
 	Driver_USART0.Control(ARM_USART_CONTROL_RX,1);
 }
-
 
 void Send_Lidar_Command(unsigned char command, unsigned char payloadsize, unsigned char payload[]) {	
 		
@@ -47,32 +105,10 @@ void Send_Lidar_Command(unsigned char command, unsigned char payloadsize, unsign
     while(Driver_USART0.GetStatus().tx_busy == 1); 				// attente buffer TX vide
 		Driver_USART0.Send(packet,9); 												//envoie du packet
 
-}		
+}	
 
 	void Temporisation_ms(int a){ 													//fonction de temporisation en ms
 		int j;
 		for (j=0; j<(16667*a);j++){
 		}
 	}
-	
-	
-
-int main (void){
-
-	unsigned char tab[5] = {0x00,0x00,0x00,0x00,0x00};
-	
-	Init_UART0();
-	
-	GPIO_SetDir(2,5,GPIO_DIR_OUTPUT); 													//P2.5 en sortie
-	GPIO_PinWrite(2,5,1);
-	
-	Send_Lidar_Command(0x82,0x05,tab);
-	
-	while(1){
-		
-			Send_Lidar_Command(0x82,0x05,tab);
-
-	}
-		
-	return 0;
-}
